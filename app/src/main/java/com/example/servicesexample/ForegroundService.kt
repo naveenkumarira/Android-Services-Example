6bef7b6011d30d47f4b612c3711df482145677b3
naveenkumarira <naveenrvkumar@gmail.com>
2022-06-16 20:32:38 +0530

    Foreground service added

---
+++ app/src/main/java/com/example/servicesexample/ForegroundService.kt
@@ -0,0 +1,56 @@
+package com.example.servicesexample
+
+import android.R
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+
+
+class ForegroundService : Service() {
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Thread {
+            Log.e("ForegroundService", "Service is running...")
+            try {
+                Thread.sleep(5000)
+                stopSelf()
+                Log.e("ForegroundService", "Foreground Service completed.")
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+        }.start()
+
+        // Create the Notification
+        val CHANNELID = "Foreground Service ID"
+        val notification: Notification.Builder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNELID,
+                CHANNELID,
+                NotificationManager.IMPORTANCE_LOW
+            )
+            getSystemService(NotificationManager::class.java).createNotificationChannel(channel)
+            Notification.Builder(this, CHANNELID)
+        } else {
+            Notification.Builder(this)
+        }
+
+        notification.apply {
+            setContentText("Foreground Service is running")
+            setContentTitle("Foreground Service enabled")
+            setSmallIcon(R.drawable.ic_lock_idle_alarm)
+        }
+
+        // Start the Foreground service with Notification. Notification is mandatory for Foreground services
+        startForeground(1001, notification.build())
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+}
--- app/src/main/java/com/example/servicesexample/MainActivity.kt
+++ app/src/main/java/com/example/servicesexample/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.servicesexample
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
@@ -13,7 +14,14 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.backgroundServiceButton.setOnClickListener {
+            //Launch Background services
             startService(Intent(this, SimpleService::class.java))
+        }
+        binding.foregroundServiceButton.setOnClickListener {
+            //Launch Foreground services
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(Intent(this, ForegroundService::class.java))
+            }
         }
     }
 }
